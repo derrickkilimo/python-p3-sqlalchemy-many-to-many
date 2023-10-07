a3f874a6c046e46155dbfecd9382a4256aa34cfc
derrickkilimo <derrick.kilimo@student.moringaschool.com>
2023-10-07 15:54:03 +0300

    many-many

--- lib/models.py
+++ lib/models.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine, func
 from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
+# Naming convention
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
@@ -10,32 +12,54 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+# Game class
 class Game(Base):
     __tablename__ = 'games'
 
-    id = Column(Integer(), primary_key=True)
-    title = Column(String())
-    genre = Column(String())
-    platform = Column(String())
-    price = Column(Integer())
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    genre = Column(String)
+    platform = Column(String)
+    price = Column(Integer)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
 
-    reviews = relationship('Review', backref=backref('game'))
+    reviews = relationship('Review', back_populates='game')
+    users = association_proxy('reviews', 'user', creator=lambda us: Review(user=us))
 
     def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title={self.title}, ' + \
-            f'platform={self.platform})'
+        return f'Game(id={self.id}, title={self.title}, platform={self.platform})'
 
+# User class
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+    reviews = relationship('Review', back_populates='user')
+    games = association_proxy('reviews', 'game', creator=lambda gm: Review(game=gm))
+
+    def __repr__(self):
+        return f'User(id={self.id}, name={self.name})'
+
+# Review class
 class Review(Base):
     __tablename__ = 'reviews'
 
-    id = Column(Integer(), primary_key=True)
-    score = Column(Integer())
-    comment = Column(String())
-    
-    game_id = Column(Integer(), ForeignKey('games.id'))
+    id = Column(Integer, primary_key=True)
+    score = Column(Integer)
+    comment = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+    game_id = Column(Integer, ForeignKey('games.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    game = relationship('Game', back_populates='reviews')
+    user = relationship('User', back_populates='reviews')
 
     def __repr__(self):
-        return f'Review(id={self.id}, ' + \
-            f'score={self.score}, ' + \
-            f'game_id={self.game_id})'
+        return f'Review(id={self.id}, score={self.score}, game_id={self.game_id})'
